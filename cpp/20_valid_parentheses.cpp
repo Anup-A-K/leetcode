354cd47ce029d2376cc1c1c457c98efaf351a7df
Anup-A-K <anuparunaruna@gmail.com>
2024-08-20 21:28:54 +0530

    This is an empty commit

---
+++ cpp/20_valid_parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> para;
+        for(char c : s) 
+        {
+            if(c == '(' || c == '[' || c == '{') 
+            {
+                para.push(c);
+            } 
+            else 
+            {
+                if(para.empty()) {
+                    return false;
+                }
+                if(c == ')' && para.top() != '(') {
+                    return false;
+                }
+                if(c == ']' && para.top() != '[') {
+                    return false;
+                }
+                if(c == '}' && para.top() != '{') {
+                    return false;
+                }
+                para.pop();
+            }
+        }
+        return para.empty();
+    }
+};
